2260fa15212614f3b3c12f9f324b7caf50c3b6f2
nithinalva <47344035+nithinalva@users.noreply.github.com>
2021-02-11 03:25:23 +0530

    confirm

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express =require('express')
+const connectDB= require('./config/db')
+const app=express() 
 
-const app=express()
+//connect the db
+connectDB();
 
-app.get('/',(req,res)=>res.send('API Running'))
+//initiallizing body parser middleware 
+app.use(express.json({extended:false}));
+
+
+
+
+
+app.get('/',(req,res)=>res.send('API Running'));
+
+
+app.use('/api/user', require('./routes/api/user'));
+
+
+app.use('/api/Authentication',require('./routes/api/auth'))
+
+app.use('/api/UserProfile',require('./routes/api/profile'))
+//defining routes
+ 
+
+
 
 const PORT=process.env.PORT|| 5000;
 
