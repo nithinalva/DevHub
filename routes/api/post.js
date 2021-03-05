070e801e8944c35f62af53798d9045a746adf98b
nithinalva <47344035+nithinalva@users.noreply.github.com>
2021-03-05 10:23:17 +0530

    confirm

---
+++ routes/api/post.js
@@ -0,0 +1,67 @@
+const express=require('express');
+const router=express.Router();
+const auth=require('../../middleware/auth')
+const {check,validationResult}= require('express-validator/check');
+const Profile =require('../../models/profile');
+const User=require('../../models/User');
+const Post=require('../../models/post');
+
+
+
+router.post('/',[auth,[
+    check('text', 'text is required')
+    .not()
+    .isEmpty(),
+
+]], async(req,res)=>{
+
+
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array() })
+    }
+
+
+try {
+ 
+ const user =await User.findById(req.user.id).select('-password');
+
+ const newPost= new Post({
+     text:req.body.text,
+     name:user.name,
+     avatar:user.avatar,
+     user:req.user.id
+ })  
+
+ const post=await newPost.save();
+ res.json(post);
+
+
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send('server Error')    
+}
+
+
+})
+
+
+//@route Get Api/Route
+//desc Get all posts
+//@access Private
+
+router.get('/',auth, async (req,res)=>{
+
+    try {
+            const posts= await Post.find().sort({date:-1});
+            res.json(posts)
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('server error')
+    }
+
+})
+
+module.exports= router;  
